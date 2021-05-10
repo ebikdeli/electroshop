682666215e25b4be8d9d1de6b9b7a1c3b86e1041
ebikdeli <ebikdeli@gmail.com>
2021-05-10 23:35:40 +0430

    Base Models created

--- mainshop/models.py
+++ mainshop/models.py
@@ -4,19 +4,42 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 from taggit.managers import TaggableManager
+from django_countries.fields import CountryField
 
 app_name = 'mainshop'
 
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
-    slug = models.SlugField()
     tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
 
 
 class Brand(models.Model):
-    name =
+    name = models.CharField(max_length=30)
+    country = CountryField(blank=True)
+    founded = models.CharField(max_length=4, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
-    pass
+    name = models.CharField(max_length=30,
+                            )
+    category = models.ForeignKey(Category,
+                                 related_name='product_category',
+                                 on_delete=models.CASCADE,
+                                 )
+    brand = models.ForeignKey(Brand,
+                              related_name='product_brand',
+                              on_delete=models.CASCADE,
+                              )
+    model = models.CharField(max_length=4, blank=True)
+    price = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.brand.name + ' ' + self.name
