c118f9dd74694e51e3694159fb510d6403b0bc07
ebikdeli <ebikdeli@gmail.com>
2021-07-04 18:11:50 +0430

    'base.py' completed

--- electroshop/settings/base.py
+++ electroshop/settings/base.py
@@ -140,13 +140,13 @@ STATICFILES_FINDERS = [
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR),
     os.path.join(BASE_DIR, 'shop', 'static'),
-    os.path.join(BASE_DIR, 'cart', 'static'),
-    os.path.join(BASE_DIR, 'checkout', 'static'),
+    os.path.join(BASE_DIR, 'cart'),
+    os.path.join(BASE_DIR, 'checkout'),
 ]
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR)
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
